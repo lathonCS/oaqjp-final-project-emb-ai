bab8b53850f2265af6ebd2cf7513f53e1455cd09
lathonCS <53703587+lathonCS@users.noreply.github.com>
2025-10-03 20:19:03 +0000

    Fixed issues

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+    This script is to handle requests from the webpage
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
+    """
+    This is to call the emotion_detetor function, and to pass in the string passed from the webpage
+    """
     text_to_analyse = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyse)
@@ -14,9 +21,11 @@ def emotion_detector_route():
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
+    dominant_emotion = response['dominant_emtion']
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return (f"For the given statement, the system response is 'anger': {anger}, "
+            f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and "
+            f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}.")
 
 @app.route("/")
 def render_index_page():
@@ -25,5 +34,3 @@ def render_index_page():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-    
--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,21 +1,21 @@
-from EmotionDetection.emotion_detection import emotion_detection
+from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
 class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detection_joy(self):
-        self.assertEqual((emotion_detection("I am glad this happened"))['dominant_emtion'], 'joy')
+        self.assertEqual((emotion_detector("I am glad this happened"))['dominant_emtion'], 'joy')
     
     def test_emotion_detection_anger(self):
-        self.assertEqual((emotion_detection("I am really mad about this"))['dominant_emtion'], 'anger')
+        self.assertEqual((emotion_detector("I am really mad about this"))['dominant_emtion'], 'anger')
 
     def test_emotion_detection_disgust(self):
-        self.assertEqual((emotion_detection("I feel disgusted just hearing about this"))['dominant_emtion'], 'disgust')
+        self.assertEqual((emotion_detector("I feel disgusted just hearing about this"))['dominant_emtion'], 'disgust')
     
     def test_emotion_detection_sadness(self):
-        self.assertEqual((emotion_detection("I am so sad about this"))['dominant_emtion'], 'sadness')
+        self.assertEqual((emotion_detector("I am so sad about this"))['dominant_emtion'], 'sadness')
     
     def test_emotion_detection_fear(self):
-        self.assertEqual((emotion_detection("I am really afraid that this will happen"))['dominant_emtion'], 'fear')
+        self.assertEqual((emotion_detector("I am really afraid that this will happen"))['dominant_emtion'], 'fear')
 
 if __name__ == '__main__':
     unittest.main()
